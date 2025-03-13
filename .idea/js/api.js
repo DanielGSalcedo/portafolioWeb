53ca90164b07263b6b359e9eb0468bfa970c9c22
Daniel Salcedo <johandanielgs@ufps.edu.co>
2025-03-13 14:15:05 -0500

    adding function edit student

--- .idea/js/api.js
+++ .idea/js/api.js
@@ -4,46 +4,65 @@ const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsIn
 
 // API Service
 const api = {
-  // Headers for API requests
-  headers: {
-    'apikey': API_KEY,
-    'Content-Type': 'application/json',
-    'Prefer': 'return=representation'
-  },
+    // Headers for API requests
+    headers: {
+        'apikey': API_KEY,
+        'Content-Type': 'application/json',
+        'Prefer': 'return=representation'
+    },
 
-  // Fetch all students
-  async getStudents() {
-    try {
-      const response = await fetch(`${API_URL}/student?select=*`, {
-        headers: this.headers
-      });
-      
-      if (!response.ok) {
-        throw new Error('Failed to fetch students');
-      }
-      
-      return await response.json();
-    } catch (error) {
-      console.error('Error fetching students:', error);
-      throw error;
+    // Fetch all students
+    async getStudents() {
+        try {
+            const response = await fetch(`${API_URL}/student?select=*`, {
+                headers: this.headers
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch students');
+            }
+
+            return await response.json();
+        } catch (error) {
+            console.error('Error fetching students:', error);
+            throw error;
+        }
+    },
+
+    async createNewStudent(student) {
+        try {
+            const response = await fetch(`${API_URL}/student`, {
+                method: 'POST',
+                headers: this.headers,
+                body: JSON.stringify(student)
+            });
+            if (!response.ok) {
+                throw new Error('Failed to add student');
+            }
+
+            return await response.json();
+        } catch (error) {
+            console.log('error adding student:', error);
+            throw error;
+        }
+
+    },
+
+    async editStudent(student) {
+        try {
+            const response = await fetch(`${API_URL}/student?code=eq.${student.id}`, {
+                method: 'PATCH',
+                headers: this.headers,
+                body: JSON.stringify(student)
+            });
+            if (!response.ok) {
+                throw new Error('Failed to edit student');
+            }
+            return await response.json();
+        } catch (error) {
+            console.error(`Error editing student with the code ${student.code}:`, error);
+            throw error;
+
+        }
     }
-  },
-  
-  async createNewStudent(student) {
-    try{
-      const response = await fetch(`${API_URL}/student`, {
-        method: 'POST',
-        headers: this.headers,
-        body: JSON.stringify(student)
-      });
-      if(!response.ok){
-        throw new Error('Failed to add student');
-      }
-      
-      return await response.json();
-    }catch (error) {
-      console.log('error adding student:', error);
-      throw error;
-    }
-  }
 }
--- .idea/js/app.js
+++ .idea/js/app.js
@@ -1,9 +1,12 @@
-document.addEventListener('DOMContentLoaded',async () => {
+document.addEventListener('DOMContentLoaded', async () => {
     // Sample student data
 
     const studentsList = document.getElementById('studentsList');
     const template = document.getElementById('studentCardTemplate');
     const registerBtn = document.getElementById('bnt_register');
+    const containerRegister = document.getElementById('register');
+    const containerHome = document.getElementById('list');
+
     // Render students
     async function renderStudents() {
         const students = await api.getStudents();
@@ -11,30 +14,53 @@ document.addEventListener('DOMContentLoaded',async () => {
         studentsList.innerHTML = '';
         students.forEach(student => {
             const clone = template.content.cloneNode(true);
-            
+
             clone.querySelector('.student-name').textContent = student.name;
-            clone.querySelector('.student-id').textContent = `ID: ${student.code}`;
+            clone.querySelector('.student-id').querySelector('span').innerText = student.code;
             clone.querySelector('.student-email').textContent = student.email;
             clone.querySelector('.student-photo').src = student.photo;
+            clone.querySelector('.student-description').textContent = student.description
             const github_Button = clone.querySelector('.student-github');
-            if(student.github_link){
+            if (student.github_link) {
                 github_Button.href = student.github_link;
-            } else{
+            } else {
                 github_Button.style.display = 'none';
             }
-            
+
             studentsList.appendChild(clone);
         });
     }
-    
+
     studentsList.addEventListener('click', (event) => {
         const githubBnt = event.target.closest('#bnt_github')
-        if(githubBnt) {
+        if (githubBnt) {
             window.open(githubBnt.href, '_blank');
         }
     })
-    
-    registerBtn.addEventListener('click',async () => {
+    studentsList.addEventListener('click', (event) => {
+        const editBnt = event.target.closest('#btn_edit')
+        const code = event.target.closest('.studentCard').querySelector('span').textContent;
+        const name = event.target.closest('.studentCard').querySelector('.student-name').textContent;
+        const email = event.target.closest('.studentCard').querySelector('.student-email').textContent;
+        const description = event.target.closest('.studentCard').querySelector('.student-description').textContent;
+        const photo = event.target.closest('.studentCard').querySelector('.student-photo').src;
+        const github_link = event.target.closest('.studentCard').querySelector('.student-github').href;
+        if (editBnt) {
+            containerHome.style.display = 'none';
+            containerRegister.style.display = 'flex';
+            containerRegister.querySelector('h2').innerText = 'Edit Student';
+            containerRegister.querySelector('#bnt_register').value = 'Save';
+            containerRegister.querySelector('#new_student_name').value =name ;
+            containerRegister.querySelector('#new_student_code').placeholder = code;
+            containerRegister.querySelector('#new_student_code').disabled = true;
+            containerRegister.querySelector('#new_student_email').value = email;
+            containerRegister.querySelector('#new_student_githubLink').value= github_link;
+            containerRegister.querySelector('#new_student_photo').value = photo;
+            containerRegister.querySelector('#new_student_description').value = description;
+        }
+    })
+
+    registerBtn.addEventListener('click', async () => {
         const name = document.getElementById('new_student_name').value;
         const code = document.getElementById('new_student_code').value;
         const email = document.getElementById('new_student_email').value;
@@ -43,12 +69,12 @@ document.addEventListener('DOMContentLoaded',async () => {
         const photo = document.getElementById('new_student_photo').value;
         const description = document.getElementById('new_student_description').value;
 
-        if(!code){
+        if (!code) {
             alert('Please enter a code');
             return;
         }
 
-        if(!name){
+        if (!name) {
             alert('Please enter a name');
             return;
         }
@@ -61,20 +87,21 @@ document.addEventListener('DOMContentLoaded',async () => {
             description
         }
         console.log(newStudent);
-        try{
+        try {
             await api.createNewStudent(newStudent);
             await renderStudents();
-        }catch (error) {
+        } catch (error) {
             console.error('Error adding student:', error);
             throw error;
         }
 
     });
 
+
     // Initial render
     await renderStudents();
-    
+
     // add student
-    
-    
+
+
 });
